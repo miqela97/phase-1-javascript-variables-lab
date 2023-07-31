805f4ef6d02dbb2fcbb36d162b19872d98f5b909
miqela97 <132955511+miqela97@users.noreply.github.com>
2023-07-31 13:21:55 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -1,8 +1,3 @@
 const companyName = "Scuber";
-console.log("is defined as a const:", typeof companyName === "string");
 let mostProfitableNeighborhood = "Chelsea";
 let companyCeo = "Susan Smith";
-console.log("mostProfitableNeighborhood is declared as equal to Chelsea:", mostProfitableNeighborhood === "Chelsea");
-console.log("mostProfitableNeighborhood is defined using let:", typeof mostProfitableNeighborhood === "string");
-console.log("companyCeo is declared as equal to Susan Smith:", companyCeo === "Susan Smith");
-console.log("companyCeo is defined using let:", typeof companyCeo === "string");
